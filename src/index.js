727f51ae0208d05f7879391ddf77d5201af3deff
MonishDeshpande <monish10102907@gmail.com>
2024-06-26 12:08:14 +0530

    Cleared up

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const cookieParser = require("cookie-parser");
 
-const ServerConfig = require('./config/serverConfig');
-const connectDB = require('./config/dbConfig');
-const userRouter = require('./routes/userRoute');
-const cartRouter = require('./routes/cartRoute');
-const authRouter = require('./routes/authRoute');
-const { isLoggedIn } = require('./validation/authValidator');
-const uploader = require('./middlewares/multerMiddleware');
-const cloudinary = require('./config/cloudinaryConfig');
-const fs = require('fs/promises');
-const productRouter = require('./routes/productRoute');
-const orderRouter = require('./routes/orderRoute');
+const ServerConfig = require("./config/serverConfig");
+const connectDB = require("./config/dbConfig");
+const userRouter = require("./routes/userRoute");
+const cartRouter = require("./routes/cartRoute");
+const authRouter = require("./routes/authRoute");
+const { isLoggedIn } = require("./validation/authValidator");
+const uploader = require("./middlewares/multerMiddleware");
+const cloudinary = require("./config/cloudinaryConfig");
+const fs = require("fs/promises");
+const productRouter = require("./routes/productRoute");
+const orderRouter = require("./routes/orderRoute");
 // const User = require('./schema/userSchema');
 
 const app = express();
@@ -23,31 +23,21 @@ app.use(express.urlencoded({ extended: true }));
 
 // Routing middleware
 // if your req route starts with /users then handle it using userRouter
-app.use('/users', userRouter); // connects the router to the server
-app.use('/carts', cartRouter);
-app.use('/auth', authRouter);
-app.use('/products', productRouter);
-app.use('/orders',orderRouter);
-app.get('/ping', isLoggedIn, (req, res) => {
-    // controller
-    console.log(req.body);
-    console.log(req.cookies);
-    return res.json({message: "pong"});
+app.use("/users", userRouter); // connects the router to the server
+app.use("/carts", cartRouter);
+app.use("/auth", authRouter);
+app.use("/products", productRouter);
+app.use("/orders", orderRouter);
+app.get("/ping", isLoggedIn, (req, res) => {
+  // controller
+  console.log(req.body);
+  console.log(req.cookies);
+  return res.json({ message: "pong" });
 });
 
-app.post('/photo', uploader.single('incomingFile'), async (req, res) => {
-    console.log(req.file);
-    const result = await cloudinary.uploader.upload(req.file.path);
-    console.log("result from cloudinary", result);
-    await fs.unlink(req.file.path);
-    return res.json({message: 'ok'});
-})
-
 app.listen(ServerConfig.PORT, async () => {
-    await connectDB();
-    console.log(`Server started at port ${ServerConfig.PORT}...!!`);
-
-    
+  await connectDB();
+  console.log(`Server started at port ${ServerConfig.PORT}...!!`);
 });
 
 // localhost:5500/users - GET
